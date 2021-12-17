e10490651ec459fae4e6b7e8a8676f7cb2e20207
Klay Clarke <klayaclarke@gmail.com>
2021-12-17 11:48:37 -0500

    create route to allow registered user to login

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 
 app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 # CREATE TABLE IN DB
@@ -21,6 +26,11 @@ class User(UserMixin, db.Model):
 
 # Line below only required once, when creating DB.
 # db.create_all()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
 @app.route('/')
@@ -42,8 +52,13 @@ def register():
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print(f'Email:{email}, Password:{password}')
+        return redirect(url_for('secrets'))
     return render_template("login.html")
 
 
